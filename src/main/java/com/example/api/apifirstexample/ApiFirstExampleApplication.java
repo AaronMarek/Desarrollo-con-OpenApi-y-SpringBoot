956de9fff8b2222e3ec1893162f8a00298a35134
AaronMarek <aaronmarekrojas@gmail.com>
2024-04-05 11:21:57 +0200

    Primer Commit

---
+++ src/main/java/com/example/api/apifirstexample/ApiFirstExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.apifirstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api/apifirstexample/ApiFirstExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.apifirstexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
